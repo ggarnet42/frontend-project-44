c58c3cbda4b1cae5f7e862fdf72d5931356361e9
ggarnet42 <dianarockroll@gmail.com>
2024-02-05 01:41:51 +0300

    add src/games/brainGcdGame.js

---
+++ src/games/brainGcdGame.js
@@ -0,0 +1,28 @@
+import playGame from '../index.js';
+import welcomeUser from '../cli.js';
+
+const findGcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return findGcd(b, a % b);
+};
+
+const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+const generateQuestionAndAnswer = () => {
+  const number1 = generateRandomNumber();
+  const number2 = generateRandomNumber();
+  const question = `${number1} ${number2}`;
+  const correctAnswer = String(findGcd(number1, number2));
+  return { question, correctAnswer };
+};
+
+export default function playBrainGcdGame() {
+  const name = welcomeUser();
+  console.log('Find the greatest common divisor of given numbers.');
+
+  const isCorrectAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
+
+  playGame(generateQuestionAndAnswer, isCorrectAnswer, name);
+}
