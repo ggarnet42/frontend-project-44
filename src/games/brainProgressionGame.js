d369b2946f4c21f5bf5587c42d10182663c39066
ggarnet42 <dianarockroll@gmail.com>
2024-02-15 23:47:35 +0300

    add src/games/brainProgressionGame.js

---
+++ src/games/brainProgressionGame.js
@@ -0,0 +1,37 @@
+import playGame from '../index.js';
+import welcomeUser from '../cli.js';
+
+const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+const generateProgression = () => {
+    let progression = [];
+    let length = Math.floor(Math.random() * 6) + 5;
+    let firstNumber = generateRandomNumber();
+    let step = generateRandomNumber();
+    
+    for (let i = 0; i < length; i++) {
+        progression.push(firstNumber + i * step);
+    }
+
+    let hiddenIndex = Math.floor(Math.random() * length);
+    let hiddenValue = progression[hiddenIndex];
+    progression[hiddenIndex] = '..';
+    
+    return { progression, hiddenIndex, hiddenValue};
+};
+
+const generateQuestionAndAnswer = () => {
+    const { progression, hiddenIndex, hiddenValue} = generateProgression(); 
+    const question = progression.join(' '); 
+    const correctAnswer = String(hiddenValue); 
+    return { question, correctAnswer };
+  };
+
+export default function playBrainProgressionGame() {
+  const name = welcomeUser();
+  console.log('What number is missing in the progression?');
+
+  const isCorrectAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
+
+  playGame(generateQuestionAndAnswer, isCorrectAnswer, name);
+}
