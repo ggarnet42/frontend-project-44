8ea0ee797bd0cc34f0a997f739634d19a3be2ed0
ggarnet42 <dianarockroll@gmail.com>
2024-02-15 23:47:08 +0300

    add src/games/brainPrimeGame.js

---
+++ src/games/brainPrimeGame.js
@@ -0,0 +1,32 @@
+import playGame from '../index.js';
+import welcomeUser from '../cli.js';
+
+const isPrime = (n) => {
+    if (n <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i += 1) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+  
+  const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+  
+  const generateQuestionAndAnswer = () => {
+    const number = generateRandomNumber();
+    const question = `${number}`;
+    const correctAnswer = isPrime(number) ? 'yes' : 'no';
+    return { question, correctAnswer };
+  };
+  
+  export default function playBrainPrimeGame() {
+    const name = welcomeUser();
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  
+    const isCorrectAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
+  
+    playGame(generateQuestionAndAnswer, isCorrectAnswer, name);
+  }
