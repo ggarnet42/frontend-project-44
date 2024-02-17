e77a5367e11339a5ea623f4ddf9e3da07c72ccc2
ggarnet42 <dianarockroll@gmail.com>
2024-02-17 18:17:07 +0300

    modified:   src/games/brainEvenGame.js

--- src/games/brainEvenGame.js
+++ src/games/brainEvenGame.js
@@ -1,34 +1,24 @@
-import readlineSync from 'readline-sync';
+import playGame from '../index.js';
 import welcomeUser from '../cli.js';
 
 function isEven(number) {
   return number % 2 === 0;
 }
 
+const generateRandomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+const generateQuestionAndAnswer = () => {
+  const number = generateRandomNumber();
+  const question = `${number}`;
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  return { question, correctAnswer };
+};
+
 export default function playBrainEvenGame() {
-  const name = welcomeUser();
-
-  process.stdout.write('Answer "yes" if the number is even, otherwise answer "no".');
-
-  let correctAnswersCount = 0;
-
-  while (correctAnswersCount < 3) {
-    const questionNumber = Math.floor(Math.random() * 20) + 1;
-    const isQuestionEven = isEven(questionNumber);
-    const correctAnswer = isQuestionEven ? 'yes' : 'no';
-
-    console.log(`Question: ${questionNumber}`);
-    const userAnswer = readlineSync.question('Your answer:').toLowerCase();
-
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-      correctAnswersCount += 1;
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
+    const name = welcomeUser();
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  
+    const isCorrectAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
+  
+    playGame(generateQuestionAndAnswer, isCorrectAnswer, name);
   }
-
-  console.log(`Congratulations, ${name}!`);
-}
